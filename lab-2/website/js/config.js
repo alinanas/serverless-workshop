bdb71479c3ed91d30b6590c0dee4858896ffaf67
Alina Nasibullina <alina.nasibullina@dataart.com>
2023-12-23 14:50:08 +0100

    init commit

---
+++ lab-2/website/js/config.js
@@ -0,0 +1,6 @@
+var configConstants = {
+    auth0: {
+        domain: 'dev-rvnsdtcx.eu.auth0.com',
+        clientId: 'pFifCf0j2wbK2erLiIGorpnyuorncL36'
+    }
+};
---
+++ lab-3/website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'dev-rvnsdtcx.eu.auth0.com',
+        clientId: 'pFifCf0j2wbK2erLiIGorpnyuorncL36'
+    },
+    apiBaseUrl: 'https://djn0gviuza.execute-api.us-east-1.amazonaws.com/dev'
+};
